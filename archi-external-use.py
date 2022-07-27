eb2d136ab13a324a58ffbd04a0f7382a9dffa5be
Claude Moreau <claude.moreau.olom@gmail.com>
2022-07-27 17:47:00 +0200

    draft 2

--- archi-external-use.py
+++ archi-external-use.py
@@ -1,2 +1,4 @@
 # https://github.com/pospisilboh/ScreenDocumentation/blob/main/screen_documentation_notebook.ipynb
 
+# find out how to take into account any new attribute I could add in an ini file for an element (map ?)
+
