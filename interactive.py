f5dbca35735fc8e50e0a38793742b544daca6a93
Claude Moreau <claude.moreau.olom@gmail.com>
2022-07-27 17:47:21 +0200

    draft 2

--- interactive.py
+++ interactive.py
@@ -34,3 +34,25 @@ for a in topnodes:
                 if s.attributes.length > 0:
                     print(s.attributes.item(0).nodeName)
                     print(s.getAttribute('xsi:type'))
+#%%
+from anytree import Node, RenderTree
+import uuid
+for n in range(10):
+    print(uuid.uuid1())
+
+ids = ['1-8fb02a8f-0d8f-11ed-b9ee-ac74b12a0cdc',
+'2-8fb051a2-0d8f-11ed-81ae-ac74b12a0cdc',
+'3-8fb051a3-0d8f-11ed-b075-ac74b12a0cdc',
+'4-8fb051a4-0d8f-11ed-bb89-ac74b12a0cdc',
+'5-8fb051a5-0d8f-11ed-aad7-ac74b12a0cdc',
+'6-8fb051a6-0d8f-11ed-9185-ac74b12a0cdc',
+'7-8fb051a7-0d8f-11ed-be2c-ac74b12a0cdc',
+'8-8fb051a8-0d8f-11ed-910f-ac74b12a0cdc',
+'9-8fb051a9-0d8f-11ed-960b-ac74b12a0cdc',
+'0-8fb051aa-0d8f-11ed-811a-ac74b12a0cdc']
+my_data = {}
+my_data[ids[0]] = Node(ids[0])
+my_data[ids[1]] = Node(ids[1], parent=my_data[ids[0]])
+for pre, fill, node in RenderTree(my_data[ids[0]]):
+    print("%s%s" % (pre, node.name))
+
