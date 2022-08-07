cf72691b6cd0d4cf0bec5f22d116877c28ecf804
Claude Moreau <claude.moreau.olom@gmail.com>
2022-08-07 17:46:50 +0200

    draft

--- archi/tools.py
+++ archi/tools.py
@@ -21,21 +21,12 @@ def processNodeAttributes(node: Node, content: conf.XMLContent) -> None:
     if node.hasAttributes():
         map = node.attributes
         for key in map.keys():
-            if map[key].localName in content.ToStore.list():
-                mlogger.debug(f'parent of the node : {node.parentNode.localName}, attr name :  {map[key].localName}, value : {map[key].value}')
-                match key:
-                    case conf.ToStore.ID.value:
-                        content.allObjects[content.currentNodeType][conf.Position.ID.value].append(map[key].value)
-                    case conf.ToStore.TYPE.value:
-                        content.allObjects[content.currentNodeType][conf.Position.TYPE.value].append(map[key].value)
-                    case conf.ToStore.PROPERTY.value:
-                        content.allObjects[content.currentNodeType][conf.Position.PROPNAME.value].append(map[key].value)
-                    case conf.ToStore.SOURCE.value:
-                        content.allObjects[content.currentNodeType][conf.Position.SOURCE.value].append(map[key].value)
-                    case conf.ToStore.TARGET.value:
-                        content.allObjects[content.currentNodeType][conf.Position.TARGET.value].append(map[key].value)
-            else:
-                mlogger.warning(f'this attribute is unknown : {map[key].localName}, add it in ATTRIBUTESOFTHENODES in order to process it.')
+            try:
+                tofind = node.parentNode.localName+"-"+map[key].localName
+                i = content.TOSTORE[content.currentNodeType].index(tofind)
+                content.allObjects[content.currentNodeType][i].append(map[key].value)
+            except KeyError as k:
+                mlogger.warning(f"'{tofind}' not found in TOSTORE, check the configuration")    
     else:
         content.somethingWentWrong == True
         mlogger.warning(f'node : {node.localName} has no attributes even though the configuration says otherwise. check GETFROMTHODES')
@@ -44,19 +35,12 @@ def processNodeAttributes(node: Node, content: conf.XMLContent) -> None:
 def processNodeValue(node: Node, content: conf.XMLContent) -> None:
     mlogger.debug(f'function processNodeValue() : node received {node.localName}. parent : {node.parentNode.localName}')
     if node.hasChildNodes():
-        i = content.NODES[content.currentNodeType].index(node.localName)
-        match (node.localName):
-            case (conf.ToStore.DOCUMENTATION.value):
-                content.allObjects[content.currentNodeType][conf.Position.DOCUMENTATION.value].append(node.firstChild.data)
-            case (conf.ToStore.NAME.value):
-                if (content.PARENTSOFTHESENODES[content.currentNodeType][i] == conf.QualifyName.PROPERTYNAME.value):
-                    content.allObjects[content.currentNodeType][conf.Position.PROPVALUE.value].append(node.firstChild.data)
-                else:
-                    content.allObjects[content.currentNodeType][conf.Position.NAME.value].append(node.firstChild.data)
-            case _:
-                mlogger.warning(f'This node is not parametized, set it up. Node name : {node.localName}. Parent : {node.parentNode.localName}')
-                
-        print(f'parent : {node.parentNode.localName}, node name : {node.localName}, value : {node.firstChild.data}')
+        try:
+            tofind = node.parentNode.localName+"-"+node.localName
+            i = content.TOSTORE[content.currentNodeType].index(tofind)
+            content.allObjects[content.currentNodeType][i].append(node.firstChild.data)
+        except KeyError as k:
+            mlogger.warning(f"'{tofind}' not found in TOSTORE, check the configuration")
     else:
         content.somethingWentWrong == True
         mlogger.warning(f'node : {node.localName} has no value even tough the configuration says otherwise. check GETFROMTHODES')
@@ -65,22 +49,17 @@ def processNodeValue(node: Node, content: conf.XMLContent) -> None:
 def processNode(node: Node, content: conf.XMLContent) -> None:
     mlogger.debug(f'function processNode() : node received {node.localName}')
     try:
-        i = 0
-        i == content.NODES[content.currentNodeType].index(node.localName)
+        tofind = node.parentNode.localName+"-"+node.localName
+        i = content.NODES[content.currentNodeType].index(tofind)
         mlogger.debug(f'function processNode() : indice to seek into NODES : {i}')
-        if (node.parentNode.localName == content.PARENTSOFTHESENODES[content.currentNodeType][i]):
-            match content.GETFROMTHESENODES[content.currentNodeType][i]:
-                case conf.ToGet.ATTR:
-                    processNodeAttributes(node, content)
-                case conf.ToGet.DATA:
-                    processNodeValue(node, content)
-        else:
-            mlogger.warning(f'unexpected parent : {node.parentNode.localName}, for node name : {node.localName}')
-    except BaseException as be:
-        mlogger.critical(f'Unexpected error in function processNode() : {type(be)}{be.args}')
+        match content.GETFROMTHESENODES[content.currentNodeType][i]:
+            case conf.ToGet.ATTR:
+                processNodeAttributes(node, content)
+            case conf.ToGet.DATA:
+                processNodeValue(node, content)
+    except KeyError as k:
+        mlogger.critical(f"'{tofind}' not found in NODES, check the configuration.")
         content.somethingWentWrong = True        
-
-
 
 
 @log_function_call
